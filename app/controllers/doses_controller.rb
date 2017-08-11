30dd8dc7fe4133436ad0f99d6d10a20b7dccd0d1
Amana Cotcamp <amanacodercamp@gmail.com>
2017-08-11 13:10:35 -0300

    few changes

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,27 @@
 class DosesController < ApplicationController
 
+  def new
+    ingredients = Ingredient.all
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail_id = @cocktail.id
 
     if @dose.save
-      redirect_to @dose.cocktail, notice: 'Dose was successfully created.'
+      redirect_to cocktails_path(@cocktail), notice: 'Dose was successfully created.'
     else
-      redirect_to @dose.cocktail, notice: 'Dose was successfully created.'
+      render "cocktails/show"
     end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@
 #   root to: "cocktails#index"
 # end
 Rails.application.routes.draw do
-  resources :cocktails, only: [:show, :index, :new, :create]
-  resources :doses, only: [:create, :destroy]
-  resources :ingredients, only: [:show]
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:create, :destroy]
+  end
 
+  resources :ingredients, only: [:show]
+  delete "doses/:id", to: "doses#destroy"
   root "cocktails#index"
 end
