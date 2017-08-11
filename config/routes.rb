62682b8dd354b4042b48da0afa8011927c557493
Amana Cotcamp <amanacodercamp@gmail.com>
2017-08-11 11:53:16 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   resources :cocktails
 
-  root to: "cocktails#index"
+#   root to: "cocktails#index"
+# end
+Rails.application.routes.draw do
+  resources :cocktails, only: [:show, :index, :new, :create]
+  resources :doses, only: [:create, :destroy]
+  resources :ingredients, only: [:show]
+
+  root "cocktails#index"
 end
